eb02dae8ce302756da1a556ecca7d866d9e45895
Daniel James <danieldragonhatcher@gmail.com>
2022-01-20 15:05:47 -0800

    different lib

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,25 +1,23 @@
-import me.jakejmattson.discordkt.api.dsl.bot
-import me.jakejmattson.discordkt.api.dsl.commands
+import dev.kord.core.Kord
+import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.on
 
-fun main() {
+suspend fun main() {
     val token = System.getenv("BOT_TOKEN")
         ?: throw Exception("Must include bot token in environment variable for bot to run")
+    val kord = Kord(token)
 
-    bot(token) {
-        prefix { "!" }
-        commands("Commands") {
-            command("ping") {
-                description = "Says pong"
-                execute {
-                    respond("pong!")
-                }
-            }
-//            command("opr") {
-//                description = "Gives the most recent opr of the given team"
-//                execute(IntegerArg) {
-//                    val team
-//                }
-//            }
-        }
+    kord.on<MessageCreateEvent> {//runs every time a message is created that our bot can read
+
+        //ignore other bots, even ourselves. We only serve humans here!
+        if(message.author?.isBot != false) return@on
+
+        //check if our command is being invoked
+        if(message.content != "!ping") return@on
+
+        //all clear, give them the pong!
+        message.channel.createMessage("pong!")
     }
+
+    kord.login()
 }
